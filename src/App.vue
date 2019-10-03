70bf0bf7a2becb032fa63e514fdce84e5ac3df92
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2019-10-03 20:30:30 +0800

    Order CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>17 Drinks</h1>
+    <img alt="Vue logo" src="./assets/17.png" />
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,194 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="header-bar">
+      <button @click="pushTempOrder" v-show="tempOrder.length===0">Create an order</button>
+    </div>
+    <div class="list">
+      <div class="order" v-for="(item,index) in tempOrder" :key="item.id">
+        <template v-if="item.state===1">
+          <h1>SAVED!</h1>
+        </template>
+        <template v-else-if="item.state===2">
+          <h1>DELETED!</h1>
+        </template>
+        <template v-else>
+          <div class="form-group">
+            <input type="text" v-model="item.name" placeholder="Name" />
+          </div>
+          <div class="form-group">
+            <input type="number" v-model="item.price" placeholder="Price" />
+          </div>
+          <div class="form-group">
+            <textarea rows="3" v-model="item.notes" placeholder="Notes"></textarea>
+          </div>
+          <div class="form-group">
+            <button @click="cancelTempOrder(index)">Cancal</button>
+            <button @click="createTempOrder(index)">Add</button>
+          </div>
+        </template>
+      </div>
+    </div>
+    <div class="list">
+      <div class="order" v-for="(item,index) in list" :key="item.id">
+        <template v-if="item.state===1">
+          <h1>SAVED!</h1>
+        </template>
+        <template v-else-if="item.state===2">
+          <h1>DELETED!</h1>
+        </template>
+        <template v-else-if="item.state===3">
+          <div class="form-group">
+            <input type="text" v-model="item.name" placeholder="Name" />
+          </div>
+          <div class="form-group">
+            <input type="number" v-model="item.price" placeholder="Price" />
+          </div>
+          <div class="form-group">
+            <textarea rows="3" v-model="item.notes" placeholder="Notes"></textarea>
+          </div>
+          <div class="form-group">
+            <!-- <button @click="cancelTempOrder(index)">Cancal</button> -->
+            <button @click="item.state=0">Save</button>
+          </div>
+        </template>
+        <template v-else>
+          <div class="title">{{ item.name }}</div>
+          <div>
+            <span>${{ item.price }}</span>
+            - {{ item.notes }}
+          </div>
+          <div class="form-group">
+            <button @click="updateOrder(index)">Update</button>
+            <button @click="deleteOrder(index)">Delete</button>
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// state 0 normal 1 saved 2 deleted 3 updating
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      list: [
+        {
+          state: 0,
+          id: "IWannaBeYourCoworker",
+          name: "高山烏龍茶",
+          price: 35,
+          notes: "無糖少冰加寒天"
+        }
+      ],
+      tempOrder: []
+    };
+  },
+  methods: {
+    createTempOrder(i) {
+      // TODO valid data
+      this.tempOrder[i].id = this.getGuid();
+      this.createOrder(this.tempOrder[i]);
+      this.tempOrder.splice(i, 1);
+    },
+    cancelTempOrder(i) {
+      this.tempOrder.splice(i, 1);
+    },
+    pushTempOrder() {
+      let tempOrder = {
+        state: 0,
+        id: null,
+        name: "",
+        price: null,
+        notes: ""
+      };
+      this.tempOrder.push(tempOrder);
+    },
+    createOrder(pkg) {
+      pkg.state = 1;
+      this.list.push(pkg);
+      setTimeout(() => {
+        const lastIndex = this.list.length - 1;
+        this.list[lastIndex].state = 0;
+      }, 1200);
+    },
+    deleteOrder(i) {
+      this.list[i].state = 2;
+      let id = this.list[i].id;
+      setTimeout(() => {
+        const deletingIndex = this.getIndxById(id);
+        console.log(deletingIndex);
+        this.list.splice(deletingIndex, 1);
+      }, 1200);
+    },
+    updateOrder(i) {
+      this.list[i].state = 3;
+    },
+    getGuid() {
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (
+          c ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+      );
+    },
+    getIndxById(id) {
+      return this.list.findIndex(item => item.id === id);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.header-bar {
+  margin: 5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  text-align: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.list {
+  opacity: 0.9;
+  text-align: left;
+  > div {
+    line-height: 2rem;
+  }
+  .order {
+    margin-bottom: 5px;
+    padding: 10px 15px;
+    border-bottom: 1px solid #eee;
+    .title {
+      font-size: 1.3rem;
+    }
+    &:hover {
+      opacity: 1;
+    }
+  }
 }
-a {
-  color: #42b983;
+.form-group {
+  width: 100%;
+  margin-bottom: 5px;
+  width: 100%;
+  display: flex;
+}
+textarea,
+input {
+  min-height: 20px;
+  padding: 2px;
+  outline: none;
+  box-sizing: border-box;
+  width: 100%;
+  border: 1px solid #eee;
+}
+button {
+  padding: 10px 15px;
+  min-width: 50%;
+  flex: 1;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  &:focus {
+    outline: none;
+  }
 }
 </style>
