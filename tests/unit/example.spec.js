4a4f54b94533853011525067464dc48e9df12cbb
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2019-10-03 21:45:58 +0800

    Add three test-case

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,54 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+describe("HelloWorld.vue", () => {
+  it("Order item should be empty", () => {
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+      data() {
+        return {
+          list: [],
+          tempOrder: []
+        };
+      }
+    });
+    expect(wrapper.contains("div.order")).toBe(false);
+  });
+  it("Create order with negative price", () => {
+    const wrapper = shallowMount(HelloWorld, {
+      data() {
+        return {
+          list: [],
+          tempOrder: [
+            {
+              state: 0,
+              id: "wrong",
+              name: "高山烏龍茶",
+              price: -1,
+              notes: "無糖少冰加寒天"
+            }
+          ]
+        };
+      }
+    });
+    expect(wrapper.contains("button:disabled")).toBe(true);
+  });
+  it("Order can be update and delete", () => {
+    const wrapper = shallowMount(HelloWorld, {
+      data() {
+        return {
+          list: [
+            {
+              state: 0,
+              id: "IWannaBeYourCoworker",
+              name: "高山烏龍茶",
+              price: 35,
+              notes: "無糖少冰加寒天"
+            }
+          ],
+          tempOrder: []
+        };
+      }
+    });
+    expect(wrapper.findAll("button").length).toEqual(3);
+  });
+});
