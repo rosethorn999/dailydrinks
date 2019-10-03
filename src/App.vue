600abf4e45cbcdf18e6519c4b3f838e45d2e9f3e
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2019-10-03 20:35:26 +0800

    Add RWD

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,38 @@ export default {
 </script>
 
 <style lang="scss">
+$pc-media: 960px;
+$pcs-media: 959px;
+$pad-media: 760px;
+$pads-media: 759px;
+$phone-media: 480px;
+$phones-media: 479px;
+
+//電腦
+@mixin pc-width() {
+  @media all and (min-width: $pc-media) {
+    @content;
+  }
+}
+//平板
+@mixin pad-width() {
+  @media all and (min-width: $pad-media) and (max-width: $pcs-media) {
+    @content;
+  }
+}
+//小平板
+@mixin small-pad-width() {
+  @media all and (min-width: $phone-media) and (max-width: $pads-media) {
+    @content;
+  }
+}
+//手機
+@mixin phone-width() {
+  @media all and (max-width: $phones-media) {
+    @content;
+  }
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +57,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  @include pc-width {
+    padding: 0 20%;
+  }
 }
 </style>
